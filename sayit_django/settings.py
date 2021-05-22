3f4de18b1608b657f19cf4140a8d8408ebea4cb5
Vignesh1357 <vinayv1357@gmail.com>
2021-05-22 13:07:23 +0530

    Settings.py chnaged

--- sayit_django/settings.py
+++ sayit_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['sayitw3ebapp.herokuapp.com']
+ALLOWED_HOSTS = ['sayitwebapp.herokuapp.com']
 
 
 # Application definition
