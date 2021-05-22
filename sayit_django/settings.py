a22c442233e41dabaf3a958c19a342da862711b5
Vignesh1357 <vinayv1357@gmail.com>
2021-05-22 12:14:24 +0530

    added env variables to heroku

--- sayit_django/settings.py
+++ sayit_django/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dj!2r7^u5d$%u=ae)3_q*ro_=!dvk05t0^(#@png)!y0s%&k)k'
-
+#SECRET_KEY = 'dj!2r7^u5d$%u=ae)3_q*ro_=!dvk05t0^(#@png)!y0s%&k)k'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sayitw3ebapp.herokuapp.com']
 
 
 # Application definition
